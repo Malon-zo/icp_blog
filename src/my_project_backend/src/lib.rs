2532b34ad3306e8ca8b2277e14ec32f6d02cfee0
Filip Flak <filipflak.2005@wp.pl>
2024-12-03 17:30:19 +0000

    Change add_blog

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -14,14 +14,15 @@ fn get_blogs() -> Vec<Blog>{
 }
 
 #[ic_cdk::update]
-fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<String, String> {
+fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<Blog, String> {
 
     if title.len() > 255 {
         return Err("Blog title is too long!".to_string())
     }
-
+    
     let new_blog: Blog = Blog::new(title, content, tags);
     BLOGS.with(|blogs: &RefCell<Vec<Blog>>| blogs.borrow_mut().push(new_blog));
 
-    Ok("Added new blog".to_string())
+    let last_blog: Blog = BLOGS.with(|blogs: &RefCell<Vec<Blog>>| blogs.borrow().last().expect("Blog list is empty!").clone());
+    Ok(last_blog)
 }
